c9c84806a584b14834f679b59f5659e5ba89940d
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2018-03-26 21:58:57 +0100

    Multiple new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,50 +3,70 @@ function shuffle(o) {
 	return o;
 };
 
+function createList(target, input) {
+	for (var i = 0; i < input.length; i++) {
+		// Elements
+		var card = document.createElement("div");
+
+		var name = document.createElement("h2");
+		var logo = document.createElement("div");
+		var desc = document.createElement("span");
+		var link = document.createElement("div");
+
+		// Load the content
+		$(card).addClass("card")
+
+		$(name)
+			.text(input[i].name)
+			.addClass("name")
+
+		if (input[i].verified == true) $(name).addClass("verified")
+
+		$(desc)
+			.text(input[i].description)
+			.addClass("description")
+
+		// Check NSFW
+		var nsfwfilter = "avatar";
+		if (input[i].nsfw == true) {
+			nsfwfilter = "avatar nsfw";
+			$(name).append('<span class="nsfw-tag">NSFW</span>');
+		}
+
+		$(logo)
+			.addClass("avatar")
+			.append('<img class="'+nsfwfilter+'" src="'+input[i].avatar+'">')
+
+		$(link)
+			.addClass("link buttons")
+			.append('<a target="_blank" href="'+input[i].link+'" class="btn green">Add this bot</a>')
+
+
+		$(card).append(logo, name, desc, link); // Append content
+		$(target).append(card); // Append result
+	}
+}
+
 window.addEventListener('load', function() {
   var target = document.getElementById("allbots");
 
   $.getJSON("/bots.json", function(json) {
     var randomlist = shuffle(json);
-    for (var i = 0; i < randomlist.length; i++) {
-      // Elements
-      var card = document.createElement("div");
+		var verified = [];
+		var therest = [];
+		var collected = [];
 
-      var name = document.createElement("h2");
-      var logo = document.createElement("div");
-      var desc = document.createElement("span");
-      var link = document.createElement("div");
-
-      // Load the content
-      $(card).addClass("card")
-
-      $(name)
-        .text(json[i].name)
-        .addClass("name")
-
-      $(desc)
-        .text(json[i].description)
-        .addClass("description")
-
-			// Check NSFW
-			var nsfwfilter = "avatar";
-			if (json[i].nsfw == true) {
-				nsfwfilter = "avatar nsfw";
-				$(name).append('<span class="nsfw-tag">NSFW</span>');
+		// Split between verified and not verified
+		for (var i = 0; i < json.length; i++) {
+			if (json[i].verified == true) {
+				verified.push(json[i]);
+			} else {
+				therest.push(json[i]);
 			}
+		}
 
-      $(logo)
-				.addClass("avatar")
-        .append('<img class="'+nsfwfilter+'" src="'+json[i].avatar+'">')
+		createList(target, shuffle(verified));
+		createList(target, shuffle(therest));
 
-      $(link)
-        .addClass("link buttons")
-        .append('<a target="_blank" href="'+json[i].link+'" class="btn green">Add this bot</a>')
-
-
-      $(card).append(logo, name, desc, link); // Append content
-      $(target).append(card); // Append result
-    }
   });
-
 });
