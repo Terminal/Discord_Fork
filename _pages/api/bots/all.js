5d464295be0badb039c11cf6fcddf50d8371a839
Leondro Lio <leondrolio@gmail.com>
2018-07-16 15:49:21 +0100

    Remove meme api, add "support" key

--- _pages/api/bots/all.js
+++
@@ -1,8 +0,0 @@
----
-permalink: "/api/bots/all.js"
----
-const bots = [
-  {% for bot in site.bots %}{
-{% include bot.json %}
-  }{% unless forloop.last %},{% endunless %}{% endfor %}
-];
--- _pages/api/servers/all.js
+++
@@ -1,8 +0,0 @@
----
-permalink: "/api/servers/all.js"
----
-const servers = [
-  {% for server in site.servers %}{
-{% include server.json %}
-  }{% unless forloop.last %},{% endunless %}{% endfor %}
-];
