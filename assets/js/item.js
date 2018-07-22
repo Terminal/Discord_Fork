875c6ab07d75dbc12d96141b982f19f896ca662a
lepon01 <leondrolio@gmail.com>
2018-07-22 15:02:54 +0100

    Remove extra space

--- assets/js/item.js
+++ assets/js/item.js
@@ -6,7 +6,6 @@
     avatar.src = '/assets/images/logo.png';
   });
 
-  
   button.addEventListener('click', (e) => {
     const discordWindow = window.open("{{ page.link }}", '_blank', `toolbar=0,width=500,height=700,top=${Math.floor(screen.height / 2) - 250},left=${Math.floor(screen.width / 2) - 350}}`);
     showModal(discordWindow);
