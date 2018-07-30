7af94d802f627ef0ae9e712d5f6a542730159e53
Leondro Lio <admin@moustacheminer.com>
2018-07-30 20:12:57 +0100

    Move script to top, add event listener

--- webpack/index.js
+++ webpack/index.js
@@ -1,21 +1,25 @@
 require('babel-polyfill');
 
 ((localStorage) => {
-  const GitHub = require('github-api');
-  let github = null;
-  const token = localStorage.getItem('github');
+  const run = () => {
+    console.log('bb');
+    const GitHub = require('github-api');
+    let github = null;
+    const token = localStorage.getItem('github');
 
-  if (token) {
-    github = new GitHub({
-      token: localStorage.getItem('github')
-    });
-  }
+    if (token) {
+      github = new GitHub({
+        token: localStorage.getItem('github')
+      });
+    }
 
-  require('smoothscroll');
-  require('./helpers/random');
-  require('./helpers/user')(github, localStorage);
-  if (document.body.dataset.lsType === 'docs') require('./pages/documentation');
-  if (document.body.dataset.lsType === 'list') require('./pages/list')(github, localStorage);
-  if (document.body.dataset.lsType === 'item') require('./pages/item.js')();
-  if (window.location.pathname === '/oauth/callback/') require('./pages/callback')(localStorage);
+    require('./helpers/random');
+    require('./helpers/user')(github, localStorage);
+    if (document.body.dataset.lsType === 'docs') require('./pages/documentation');
+    if (document.body.dataset.lsType === 'list') require('./pages/list')(github, localStorage);
+    if (document.body.dataset.lsType === 'item') require('./pages/item.js')();
+    if (window.location.pathname === '/oauth/callback/') require('./pages/callback')(localStorage);
+  };
+
+  document.addEventListener('DOMContentLoaded', run);
 })(localStorage);
