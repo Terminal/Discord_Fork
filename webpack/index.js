01583b4c1c0c5b7455887497dcbf4d5a756d7740
lepon01 <katie@discordmail.com>
2018-08-02 01:00:24 +0100

    wrap the documentation inside a function

--- webpack/index.js
+++ webpack/index.js
@@ -14,7 +14,7 @@ require('babel-polyfill');
 
     require('./helpers/random');
     require('./helpers/user')(github, localStorage);
-    if (document.body.dataset.lsType === 'docs') require('./pages/documentation');
+    if (document.body.dataset.lsType === 'docs') require('./pages/documentation')();
     if (document.body.dataset.lsType === 'list') require('./pages/list')(github, localStorage);
     if (document.body.dataset.lsType === 'item') require('./pages/item.js')(github, localStorage);
     if (window.location.pathname === '/oauth/login/') require('./pages/login')(localStorage);
--- webpack/pages/documentation.js
+++ webpack/pages/documentation.js
@@ -1,14 +1,16 @@
-const navside = document.getElementById('navside');
-const menuIcon = document.getElementById("menu-icon");
+module.exports = () => {
+  const navside = document.getElementById('navside');
+  const menuIcon = document.getElementById("menu-icon");
 
-if (navside && menuIcon) {
-  menuIcon.addEventListener('click', () => {
-    navside.style.transform = 'translateX(0px)';
-  });
-  
-  document.addEventListener("click", (event) => {
-    if (!event.target.closest(".nav-container")) {
-      navside.style.transform = 'translateX(-250px)';
-    }
-  });  
-}
+  if (navside && menuIcon) {
+    menuIcon.addEventListener('click', () => {
+      navside.style.transform = 'translateX(0px)';
+    });
+    
+    document.addEventListener("click", (event) => {
+      if (!event.target.closest(".nav-container")) {
+        navside.style.transform = 'translateX(-250px)';
+      }
+    });  
+  }
+};
