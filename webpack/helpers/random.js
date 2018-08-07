5c7a3154c8e28725bf8a2189f696f7543ca138da
lepon01 <katie@discordmail.com>
2018-08-07 01:50:21 +0100

    Move random messages outside of webpack to be easily edited

--- webpack/helpers/random.js
+++ webpack/helpers/random.js
@@ -2,19 +2,13 @@
  * Set a random message to randomMessages
  */
 (() => {
-  const messages = [
-    "One, two, three, let’s go\n저 우주 위로\n날아갈 듯 춤추러 가 Hey\nLet's dance the night away",
-    "jQuery free - Yes it's possible!",
-    "Compatible with Microsoft Internet Explorer 6 and Netscape Navigator 5.0 and above",
-    "Built for Windows XP - Compatible with Windows Vista",
-    "jyp",
-    "<blockquote>{{ random.message }}</blockquote> - {{ random.author }}",
-    "Made entirely of Document.createElement('div')",
-    "BLACKPINK in your area"
-  ];
-
   const randomMessages = document.getElementById('randomMessages');
   if (randomMessages) {
-    randomMessages.innerText = messages[Math.floor(messages.length * Math.random())]
+    fetch('/assets/json/random.json')
+      .then(data => data.json())
+      .then((data) => {
+        // innerText thanks
+        randomMessages.innerText = data[Math.floor(data.length * Math.random())];
+      });
   }
 })();
