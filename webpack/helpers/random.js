c86c09b6be541249ffbea5ad445398a7294ac4a0
lepon01 <katie@discordmail.com>
2018-09-13 22:24:03 +0100

    Say thanks to the people who block adverts

--- webpack/helpers/random.js
+++ webpack/helpers/random.js
@@ -2,13 +2,21 @@
  * Set a random message to randomMessages
  */
 (() => {
-  const randomMessages = document.getElementById('randomMessages');
-  if (randomMessages) {
+  const adverts = document.getElementById('adverts');
+  const randomMessage = document.getElementById('randomMessage');
+  if (adverts) {
     fetch('/assets/json/random.json')
       .then(data => data.json())
       .then((data) => {
         // innerText thanks
-        randomMessages.innerText = data[Math.floor(data.length * Math.random())];
+        adverts.innerText = data[Math.floor(data.length * Math.random())];
+
+        const id = setInterval(() => {
+          if (adverts.offsetHeight === 0) {
+            randomMessage.classList.remove('hidden');
+            clearInterval(id);
+          }
+        }, 20);
       });
   }
 })();
