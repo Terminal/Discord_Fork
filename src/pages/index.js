92294c603a8c457f09419032c39da7c3a75697eb
lepon01 <katie@discordmail.com>
2018-09-18 01:36:59 +0100

    Add a bodge to fix the avatar problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,43 +2,28 @@ import React from 'react'
 import Card from './../components/Card'
 import Cards from './../components/Cards'
 import SiteLayout from './../components/SiteLayout'
-import { StaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query HomepageQuery {
-        allMarkdownRemark(filter: {fields: {template: { eq: "bots" }}}) {
-          edges {
-            node {
-              fields {
-                filename
-                template
-              }
-              frontmatter {
-                avatar
-                pagename
-                description
-                link
-                nsfw
-                github {
-                  owner
-                  repo
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={({
-      allMarkdownRemark: {
-        edges
-      }
-    }) => (
+export default class Homepage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      render: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      render: true
+    });
+  }
+
+  render() {
+    return (
       <SiteLayout>
         <Cards>
-          {edges
+          {this.props.data.allMarkdownRemark.edges
             .map((edge) => {
               edge.score = Math.random()
               if (edge.node.frontmatter.github) edge.score += 1;
@@ -48,6 +33,32 @@ export default ({ children }) => (
             .map(edge => <Card key={edge.node.id} post={edge.node}/>)}
         </Cards>
       </SiteLayout>
-    )}
-  />
-)
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(filter: {fields: {template: { eq: "bots" }}}) {
+      edges {
+        node {
+          fields {
+            filename
+            template
+          }
+          frontmatter {
+            avatar
+            pagename
+            description
+            link
+            nsfw
+            github {
+              owner
+              repo
+            }
+          }
+        }
+      }
+    }
+  }
+`
