6acc5fa30b75943e7d8f76c3aca20ebbd0dc65bd
lepon01 <katie@discordmail.com>
2018-09-22 15:29:07 +0100

    move the shuffler outside the return

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,47 +2,31 @@ import React from 'react';
 import Link from 'gatsby-link'
 import Avatar from './../Avatar'
 
-export default class Homepage extends React.Component {
-  constructor() {
-    super();
+export default ({ post }) => {
+  const githubButton = post.frontmatter.github
+    && post.frontmatter.github.owner
+    && post.frontmatter.github.repo 
+      ? <a href={`https://github.com/${post.frontmatter.github.owner}/${post.frontmatter.github.repo}`} rel="noopener noreferrer" target="_blank">GitHub</a>
+      : null
 
-    this.state = {
-      render: false
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      render: !this.state.render
-    });
-  }
-
-  render() {
-    const githubButton = this.props.post.frontmatter.github
-      && this.props.post.frontmatter.github.owner
-      && this.props.post.frontmatter.github.repo 
-        ? <a href={`https://github.com/${this.props.post.frontmatter.github.owner}/${this.props.post.frontmatter.github.repo}`} rel="noopener noreferrer" target="_blank">GitHub</a>
-        : null
-
-    return (
-      <section className="card">
-        <div className="avatar">
-          <Avatar post={this.props.post}></Avatar>
-        </div>
-        <div className="card-content">
-          <Link to={`/${this.props.post.fields.template}/${this.props.post.fields.filename}`}>
-            <h4 className="title">
-              {this.props.post.frontmatter.pagename}
-              {this.props.post.frontmatter.nsfw ? <span className="nsfw-tag">NSFW</span> : null }
-            </h4>
-          </Link>
-          <span className="description">{this.props.post.frontmatter.description}</span>
-        </div>
-        <div className="footer">
-          <a href={this.props.post.frontmatter.link}>Invite</a>
-          {githubButton}
-        </div>
-      </section>
-    )
-  }
+  return (
+    <section className="card">
+      <div className="avatar">
+        <Avatar post={post}></Avatar>
+      </div>
+      <div className="card-content">
+        <Link to={`/${post.fields.template}/${post.fields.filename}`}>
+          <h4 className="title">
+            {post.frontmatter.pagename}
+            {post.frontmatter.nsfw ? <span className="nsfw-tag">NSFW</span> : null }
+          </h4>
+        </Link>
+        <span className="description">{post.frontmatter.description}</span>
+      </div>
+      <div className="footer">
+        <a href={post.frontmatter.link}>Invite</a>
+        {githubButton}
+      </div>
+    </section>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,37 +4,21 @@ import Cards from './../components/Cards'
 import SiteLayout from './../components/SiteLayout'
 import { graphql } from "gatsby"
 
-export default class Homepage extends React.Component {
-  constructor() {
-    super();
+export default ({ data }) => {
+  const shuffle = data.allMarkdownRemark.edges.map((edge) => {
+    edge.score = Math.random()
+    if (edge.node.frontmatter.github) edge.score += 1;
+    return edge
+  })
+  .sort((a, b) => b.score - a.score)
 
-    this.state = {
-      render: false
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      render: !this.state.render
-    });
-  }
-
-  render() {
-    return (
-      <SiteLayout>
-        <Cards>
-          {this.props.data.allMarkdownRemark.edges
-            .map((edge) => {
-              edge.score = Math.random()
-              if (edge.node.frontmatter.github) edge.score += 1;
-              return edge
-            })
-            .sort((a, b) => b.score - a.score)
-            .map(edge => <Card key={edge.node.fields.filename} post={edge.node}/>)}
-        </Cards>
-      </SiteLayout>
-    )
-  }
+  return (
+    <SiteLayout>
+      <Cards>
+        {shuffle.map(edge => <Card key={edge.node.fields.filename} post={edge.node}/>)}
+      </Cards>
+    </SiteLayout>
+  )
 }
 
 export const pageQuery = graphql`
