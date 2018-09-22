8edd7b06514368ff0d70f6f5b3905223e4df847d
lepon01 <katie@discordmail.com>
2018-09-22 16:02:21 +0100

    Stop using canonical urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,12 +38,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-catch-links',
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://discordbots.co.uk`,
-      },
-    },
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify'
   ],
