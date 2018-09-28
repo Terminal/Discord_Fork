a0ad963a1358df010f3c0440bd2c6d119c7d06eb
lepon01 <katie@discordmail.com>
2018-09-28 23:41:04 +0100

    Have a callback called "base64callback" to return a 128*128 png image

--- node/downloadImages.js
+++ node/downloadImages.js
@@ -1,7 +1,7 @@
-const request = require('request')
-const sharp = require('sharp')
-const path = require('path')
-const fs = require('fs')
+const request = require('request');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 const sizes = [
   1024,
@@ -9,9 +9,9 @@ const sizes = [
   256,
   128,
   64
-]
+];
 
-module.exports = (node) => {
+module.exports = (node, base64callback) => {
   const imageError = (size) => {
     fs.createReadStream(
       path.join(__dirname, '..', 'static', 'assets', 'images', 'logo', `logo${size}.png`)
@@ -20,11 +20,11 @@ module.exports = (node) => {
         fs.createWriteStream(
           path.join(__dirname, '..', 'public', 'userassets', `${node.fields.filelink}-${size}.png`)
         )
-      )
-  }
+      );
+  };
   
   if (node.frontmatter.avatar) {
-    const sharpreader = sharp()
+    const sharpreader = sharp();
   
     sizes.forEach((size) => {
       sharpreader
@@ -33,9 +33,25 @@ module.exports = (node) => {
         .toFile(path.join(__dirname, '..', 'public', 'userassets', `${node.fields.permalink}-${size}.png`))
         .catch(() => {
           // If there is an error resizing a specific image, copy the default image for every size
-          imageError(size)
+          imageError(size);
+        });
+    });
+
+    if (typeof base64callback === 'function') {
+      sharpreader
+        .clone()
+        .resize(128, 128)
+        .png()
+        .toBuffer()
+        .then(data => {
+          base64callback(`data:image/png;base64,${data.toString('base64')}`);
         })
-    })
+        .catch(() => {
+          const invalidAvatar = fs.readFileSync(path.join(__dirname, '..', 'static', 'assets', 'images', 'logo', 'logo128.png'));
+          const invalidAvatarBuffer = new Buffer(invalidAvatar);
+          base64callback(`data:image/png;base64,${invalidAvatarBuffer.toString('base64')}`);
+        });
+    }
     
     request({
       url: node.frontmatter.avatar,
@@ -43,12 +59,12 @@ module.exports = (node) => {
     })
       .on('error', () => {
         // If there is an error requesting an image, copy the default image for every size
-        sizes.forEach(size => imageError(size))
+        sizes.forEach(size => imageError(size));
       })
-      .pipe(sharpreader)
+      .pipe(sharpreader);
   } else {
     // If there is no avatar, copy the default image for every size
-    sizes.forEach(size => imageError(size))
+    sizes.forEach(size => imageError(size));
   }
   
-}
+};
