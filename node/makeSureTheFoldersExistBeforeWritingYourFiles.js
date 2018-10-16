e7f034ab3ca2d171204c12f5c435a3a0d6305d92
lepon01 <katie@discordmail.com>
2018-10-17 00:11:08 +0100

    Add some "next to useless" comments

--- node/makeSureTheFoldersExistBeforeWritingYourFiles.js
+++ node/makeSureTheFoldersExistBeforeWritingYourFiles.js
@@ -4,7 +4,9 @@ const mkdirp = require('mkdirp');
 const config = require('../gatsby-config');
 
 module.exports = (node) => {
+  // For each folder that needs to be created
   config.siteMetadata.foldersToClear.forEach((folder) => {
+    // Grab a path to that folder, and create it
     const location = path.parse(path.join(__dirname, '..', 'public', folder, node.fields.filelink));
     mkdirp.sync(location.dir);
   });
--- node/makeTheSvgIntoAPngPleaseThankYou.js
+++ node/makeTheSvgIntoAPngPleaseThankYou.js
@@ -2,6 +2,7 @@ const sharp = require('sharp');
 const path = require('path');
 
 module.exports = (node, svg) => {
+  // Turn the input SVG into a PNG
   sharp(Buffer.from(svg))
     .png()
     .toFile(path.join(__dirname, '..', 'public', 'api', `${node.fields.filelink}.png`))
