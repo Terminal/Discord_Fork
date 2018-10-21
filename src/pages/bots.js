b1bede8ad36c2fa0bb82bc407161a9d336786f7c
lepon01 <katie@discordmail.com>
2018-10-21 02:16:15 +0100

    Add a box to search for bots in the bots box

--- src/pages/bots.js
+++ src/pages/bots.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import { ItemPropType } from './../proptypes';
 import Card from './../components/Card';
 import Cards from './../components/Cards';
@@ -13,42 +14,65 @@ export default class BotsHomepage extends React.Component {
     super(props);
 
     this.shuffleTheBots = this.shuffleTheBots.bind(this);
+    this.searchBoxChange = this.searchBoxChange.bind(this);
 
     if (typeof window === 'undefined') {
       this.state = {
-        shuffle: this.shuffleTheBots()
+        shuffle: this.shuffleTheBots(),
+        display: this.shuffleTheBots()
       };
     } else {
       this.state = {
-        shuffle: []
+        shuffle: [],
+        display: [],
       };
     }
   }
 
   shuffleTheBots() {
     const seen = {};
-    const items = this.props.data.allMarkdownRemark.edges.map((edge) => {
+    return this.props.data.allMarkdownRemark.edges.map((edge) => {
       edge.score = Math.random();
       if (edge.node.frontmatter.github) edge.score += 1;
       if (edge.node.fields.locale === this.props.pageContext.locale) edge.score += 10;
       return edge;
     })
-      .sort((a, b) => b.score - a.score);
+      .sort((a, b) => b.score - a.score)
+      .filter((item) => {
+        if (seen.hasOwnProperty(item.node.fields.filename)) {
+          return false;
+        }
+        seen[item.node.fields.filename] = true;
+        return true;
+      });
+  }
 
-    const filtered = items.filter((item) => {
-      if (seen.hasOwnProperty(item.node.fields.filename)) {
-        return false;
-      }
-      seen[item.node.fields.filename] = true;
-      return true;
-    });
-
-    return filtered;
+  searchBoxChange(e) {
+    const target = e.target;
+    if (target.value === '') {
+      this.setState({
+        display: this.state.shuffle
+      });
+    } else {
+      this.setState({
+        display: this.state.shuffle.filter((item) => {
+          let text = [
+            item.node.frontmatter.pagename,
+            item.node.frontmatter.description
+          ];
+  
+          if (text.join(' ').toLowerCase().indexOf(target.value) === -1) return false;
+          return true;
+        })
+      });
+    }
   }
 
   componentDidMount() {
+    const shuffle = this.shuffleTheBots();
     this.setState({
-      shuffle: this.shuffleTheBots()
+      display: shuffle,
+      shuffle,
     });
   }
 
@@ -56,8 +80,15 @@ export default class BotsHomepage extends React.Component {
     return (
       <SiteLayout locale={this.props.pageContext.locale} type="bots">
         <Global />
+        <div className="center">
+          <FormattedMessage id="intro.search">
+            {((placeholder) => (
+              <input placeholder={placeholder} onChange={this.searchBoxChange}></input>
+            ))}
+          </FormattedMessage>
+        </div>
         <Cards>
-          { this.state.shuffle.length === 0 ? <Loading /> : this.state.shuffle.map(edge => <Card key={edge.node.fields.filename} post={edge.node}/>)}
+          { this.state.display.length === 0 ? <Loading /> : this.state.display.map(edge => <Card key={edge.node.fields.filename} post={edge.node}/>)}
         </Cards>
       </SiteLayout>
     );
