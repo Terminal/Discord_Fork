dcc7c82872aa865a557213bbaa7ecbb16c7591e7
Leondro Lio <github@leondrolio.com>
2020-02-19 05:25:17 +0000

    Add links into header

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -5,8 +5,13 @@ import SitePadSides from '../SitePadSides'
 const SiteHeader = () => {
   return (
     <header className={styles.header}>
-      <SitePadSides>
-        <span>DiscordFork</span>
+      <SitePadSides className={styles.container}>
+        <span className={styles.title}>DiscordFork</span>
+        <div className={styles.links}>
+          <a href="#noop">Home</a>
+          <a href="#noop">🔍 Search</a>
+          <a href="#noop">Login</a>
+        </div>
       </SitePadSides>
     </header>
   )
