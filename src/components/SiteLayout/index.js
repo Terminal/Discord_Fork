20566ebcb4a0cc8e527b1e0b96073bb270f6f36a
Leondro Lio <github@leondrolio.com>
2020-02-19 06:08:48 +0000

    Move import location

--- src/components/SiteLayout/index.js
+++ src/components/SiteLayout/index.js
@@ -3,6 +3,8 @@ import SiteHeader from "../SiteHeader"
 import SiteFooter from '../SiteFooter'
 import styles from './styles.module.scss'
 
+import '../../scss/global.scss';
+
 const SiteLayout = ({ children, data, gap = true }) => {
   return (
     <div className={`${styles.layout} ${gap && styles.gap}`}>
